bb036fc43929c02785f53d7207482c6d0abb5a59
Alexander Dubov <sannyday@gmail.com>
2025-09-27 19:45:17 +0300

    add fonts

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,21 +9,21 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a packages from fedora repos
-dnf5 -y install ecryptfs-utils
-
 # this installs a packages from COPR repos
 dnf5 -y copr enable solopasha/hyprland
-dnf5 -y copr enable che/nerd-fonts
 dnf5 -y copr enable tofik/nwg-shell
 dnf5 -y copr enable erikreider/SwayNotificationCenter
 dnf5 -y copr enable errornointernet/quickshell
-dnf5 -y install hypridle hyprland hyprlock rofi-wayland seatd SwayNotificationCenter waybar swww mpvpaper cliphist quickshell nerd-fonts
+dnf5 -y copr enable aquacash5/nerd-fonts
+
+readarray -t pkgs < <(cat /ctx/fonts /ctx/hypr)
+dnf5 -y install ${pkgs[*]}
+
 dnf5 -y copr disable solopasha/hyprland
-dnf5 -y copr disable che/nerd-fonts
 dnf5 -y copr disable tofik/nwg-shell
 dnf5 -y copr disable erikreider/SwayNotificationCenter
 dnf5 -y copr disable errornointernet/quickshell
+dnf5 -y copr disable aquacash5/nerd-fonts
 
 # install VirtualBox
 /ctx/virtualbox.sh
